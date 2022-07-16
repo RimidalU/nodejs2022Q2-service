906c1b3ea7cfe8f8c847c057ec080e98c69f4b86
Uladzimir Stankevich <stankevichuuladimiru@gmail.com>
2022-07-16 23:39:20 +0300

    feat: add base controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ArtistController } from './artist/artist.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, ArtistController],
   providers: [AppService],
 })
 export class AppModule {}
---
+++ src/artist/artist.controller.ts
@@ -0,0 +1,45 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { CreateArtistDto } from './dto/create-artist.dto';
+import { UpdateArtistDto } from './dto/update-artist.dto';
+
+@Controller('artist')
+export class ArtistController {
+  @Get()
+  getaAll() {
+    return 'getaAll';
+  }
+
+  @Get(':id')
+  getaOne(@Param('id') id: string) {
+    return 'getaOne__' + id;
+  }
+
+  @Post()
+  create(@Body() createArtistDto: CreateArtistDto): string {
+    return `
+    name: ${createArtistDto.name} 
+    grammy: ${createArtistDto.grammy}
+    `;
+  }
+
+  @Put(':id')
+  update(@Param('id') id, @Body() updateArtistDto: UpdateArtistDto): string {
+    return `Updated  ${updateArtistDto.name}` + id;
+  }
+
+  @Delete(':id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  remove(@Param('id') id: string) {
+    return 'Deleted  ' + id;
+  }
+}
