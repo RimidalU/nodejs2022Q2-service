745e15d2fff63fcb77294671efd1b33db1dc626b
Uladzimir Stankevich <stankevichuuladimiru@gmail.com>
2022-07-17 23:59:19 +0300

    feat: add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
+import { readFile } from 'fs/promises';
+import { parse } from 'yaml';
+import { dirname, join } from 'path';
 import { AppModule } from './app.module';
 import 'dotenv/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+
+  const rootDirname = dirname(__dirname);
+  const DOC_API = await readFile(join(rootDirname, 'doc', 'api.yaml'), 'utf-8');
+  const document = parse(DOC_API);
+
+  SwaggerModule.setup('doc', app, document);
   await app.listen(process.env.PORT || 4000);
 }
 bootstrap();
