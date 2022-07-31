1c32f4559d966d47426792b2bad046012e1b03be
Uladzimir Stankevich <stankevichuuladimiru@gmail.com>
2022-07-31 17:35:16 +0300

    feat: refactor user service

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,88 +1,110 @@
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdatePasswordDto } from './dto/update-password.dto';
-import { IUser } from './user.interface';
+import { CreateUserDto } from './dto/create-user.dto'
+import { UpdatePasswordDto } from './dto/update-password.dto'
+import { IUser } from './user.interface'
 import {
   BadRequestException,
   ForbiddenException,
   Injectable,
   NotFoundException,
-} from '@nestjs/common';
-import { v4, validate } from 'uuid';
-import inMemoryDbService from 'src/in-memory-db/in-memory-db.service';
-
+} from '@nestjs/common'
+import { v4, validate } from 'uuid'
+import inMemoryDbService from 'src/in-memory-db/in-memory-db.service'
+import { InjectRepository } from '@nestjs/typeorm'
+import { Repository } from 'typeorm'
+import { UserEntity } from './entities/user.entity'
+import { timeStamp } from 'console'
 @Injectable()
 export class UserService {
-  private readonly users: IUser[];
-  constructor() {
-    this.users = inMemoryDbService.users;
+  // private readonly users: IUser[]
+  constructor(
+    @InjectRepository(UserEntity)
+    private userRepository: Repository<UserEntity>,
+  ) {
+    // this.users = inMemoryDbService.users;
   }
 
-  async getOne(id: string): Promise<IUser> {
-    if (!validate(id)) {
-      throw new BadRequestException();
-    }
-    const currentUser = await this.users.find((user) => user.id === id);
-    if (!currentUser) {
-      throw new NotFoundException();
-    }
+  async getOne(id: string): Promise<Omit<UserEntity, 'password' | 'toResponse'>> {
+
+    const user = await this.userRepository.findOne({ where: { id: id } })
+    if (user) return user.toResponse()
+    throw new NotFoundException(`User with id = ${id} was not found`)
+
+    // if (!validate(id)) {
+    //   throw new BadRequestException();
+    // }
+    // const currentUser = await this.users.find((user) => user.id === id);
+    // if (!currentUser) {
+    //   throw new NotFoundException();
+    // }
+    // const userOutput = {
+    //   id: currentUser.id,
+    //   version: currentUser.version,
+    //   login: currentUser.login,
+    //   createdAt: currentUser.createdAt,
+    //   updatedAt: currentUser.updatedAt,
+    // };
+    // return userOutput;
+  }
+
+  async getAll(): Promise<Omit<UserEntity, 'password' | 'toResponse'>[]> {
+    const users = await this.userRepository.find()
+    return users.map((user) => user.toResponse())
+  }
+
+  async create(userDto: CreateUserDto): Promise<Omit<UserEntity, 'password' | 'toResponse'>> {
+
+    await this.isLoginExists(userDto.login)
+
+    const version = 1
+    const createdAt = Number(Date.now())
+    const updatedAt = Number(Date.now())
+
     const userOutput = {
-      id: currentUser.id,
-      version: currentUser.version,
-      login: currentUser.login,
-      createdAt: currentUser.createdAt,
-      updatedAt: currentUser.updatedAt,
-    };
-    return userOutput;
-  }
-
-  async getAll(): Promise<IUser[]> {
-    return this.users;
-  }
-
-  async create(userDto: CreateUserDto): Promise<IUser> {
-    const version = 1;
-    const createdAt = Number(Date.now());
-    const updatedAt = Number(Date.now());
-
-    const userOutput = {
-      id: v4(),
+      // id: v4(),
       version,
       login: userDto.login,
       createdAt,
       updatedAt,
-    };
-
-    await this.users.push({ ...userOutput, password: userDto.password });
-    return userOutput;
+    }
+    const createdUser = this.userRepository.create(userOutput)
+    return (await (await this.userRepository.save(createdUser)).toResponse())
+    // await this.users.push({ ...userOutput, password: userDto.password });
+    // return userOutput;
   }
 
   async delete(id: string): Promise<void> {
-    if (!validate(id)) {
-      throw new BadRequestException();
-    }
-    const currentUser = await this.users.find((user) => user.id === id);
-    if (!currentUser) {
-      throw new NotFoundException();
+    const result = await this.userRepository.delete(id)
+    if (result.affected === 0) {
+      throw new NotFoundException(`User with id = ${id} was not found`)
     }
 
-    const index = await this.users.findIndex((user) => user.id === id);
-    this.users.splice(index, 1);
+    // if (!validate(id)) {
+    //   throw new BadRequestException();
+    // }
+    // const currentUser = await this.users.find((user) => user.id === id);
+    // if (!currentUser) {
+    //   throw new NotFoundException();
+    // }
+
+    // const index = await this.users.findIndex((user) => user.id === id);
+    // this.users.splice(index, 1);
   }
 
-  async update(id: string, userDto: UpdatePasswordDto): Promise<IUser> {
-    if (!validate(id)) {
-      throw new BadRequestException();
-    }
+  async update(id: string, userDto: UpdatePasswordDto): Promise<Omit<UserEntity, 'password' | 'toResponse'>> {
+    // if (!validate(id)) {
+    //   throw new BadRequestException();
+    // }
+    const currentUser = await this.userRepository.findOne({ where: { id } })
 
-    const currentUser = await this.users.find((user) => user.id === id);
+    // const currentUser = await this.users.find((user) => user.id === id);
     if (!currentUser) {
-      throw new NotFoundException();
+      throw new NotFoundException(`User with id = ${id} was not found`)
     }
 
-    const index = await this.users.findIndex((user) => user.id === id);
+    // const index = await this.users.findIndex((user) => user.id === id)
 
     if (currentUser.password !== userDto.oldPassword) {
-      throw new ForbiddenException();
+      throw new ForbiddenException('Old password is wrong')
     }
 
     const userOutput = {
@@ -91,10 +113,28 @@ export class UserService {
       login: currentUser.login,
       createdAt: currentUser.createdAt,
       updatedAt: Number(Date.now()),
-    };
+    }
 
-    this.users[index] = { ...userOutput, password: userDto.newPassword };
+    // this.users[index] = { ...userOutput, password: userDto.newPassword };
+    // return userOutput;
+    const udatedUser = { ...userOutput, password: userDto.newPassword }
 
-    return userOutput;
+    return (
+      await this.userRepository.save(
+        this.userRepository.create(udatedUser),
+      )
+    ).toResponse()
+  }
+  async findbyLogin(login: string){
+    const user = await this.userRepository.findOne({where: {login}})
+    if (user) return user
+  }
+
+  async isLoginExists(login: string) {
+    const user = await this.findbyLogin(login)
+    if (user)
+      throw new BadRequestException(
+        `User with login = ${login} alredy exist`)
+
   }
 }
